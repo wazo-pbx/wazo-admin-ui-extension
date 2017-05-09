8cb28b683f5c127a788adc792236800867c14938
Sylvain Boily <sylvainboilydroid@gmail.com>
2017-05-09 17:48:43 -0400

    rename path, add packaging

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 
     entry_points={
         'wazo_admin_ui.plugins': [
-            'extension = extension.plugin:Plugin',
+            'extension = wazo_plugind_admin_ui_extension_official.plugin:Plugin',
         ]
     }
 )
