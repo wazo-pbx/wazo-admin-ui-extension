b46e829c655ab543037d384c52c21362bbeba4d0
Francois Blackburn <fblackburn@wazo.community>
2018-02-28 08:50:17 -0500

    form: instanciate InputRequired
    Since we do not really use wtform validators, the instanciation or not
    doesn't matter, but it is a styling issue

--- wazo_plugind_admin_ui_extension_official/form.py
+++ wazo_plugind_admin_ui_extension_official/form.py
@@ -17,16 +17,16 @@ from wazo_admin_ui.helpers.form import BaseForm
 
 
 class ExtensionForm(BaseForm):
-    exten = StringField(l_('Extension'), validators=[InputRequired])
-    context = SelectField(l_('Context'), validators=[InputRequired], choices=[])
+    exten = StringField(l_('Extension'), validators=[InputRequired()])
+    context = SelectField(l_('Context'), validators=[InputRequired()], choices=[])
     submit = SubmitField(l_('Submit'))
 
 
 class FeaturesForm(BaseForm):
     id = HiddenField()
     enabled = BooleanField(l_('Enabled'), default=False)
-    feature = StringField(l_('Feature'), validators=[InputRequired])
-    exten = StringField(l_('Extension'), validators=[InputRequired])
+    feature = StringField(l_('Feature'), validators=[InputRequired()])
+    exten = StringField(l_('Extension'), validators=[InputRequired()])
 
 
 class ExtensionFeaturesForm(BaseForm):
@@ -35,5 +35,5 @@ class ExtensionFeaturesForm(BaseForm):
 
 
 class ExtensionDestinationForm(BaseForm):
-    exten = StringField(l_('Extension'), validators=[InputRequired])
-    context = StringField(l_('Context'), validators=[InputRequired])
+    exten = StringField(l_('Extension'), validators=[InputRequired()])
+    context = StringField(l_('Context'), validators=[InputRequired()])
